9649d1ccd84f5ca0d030bfa0d6552123f131d08e
Nisha S <nishaamutha18@gmail.com>
2025-10-04 12:25:07 +0530

    Add files via upload

---
+++ python_outputs/S_Nisha_II_IT/module_7.py
@@ -0,0 +1,28 @@
+                  MODULE-7
+1. Testing Priority Queue:
+Popped task: task2
+Popped task: task3
+
+2. Testing Advanced Trie:
+Words with prefix 'hel': ['hello', 'help']
+Metadata for 'hello': {'frequency': 5}
+
+3. Testing Graph Algorithms:
+Shortest distances from A: {'C': 2, 'B': 4, 'A': 0, 'D': 9}
+
+4. Testing Sorting Algorithms:
+Original: [64, 34, 25, 12, 22, 11, 90]
+Radix sorted: [11, 12, 22, 25, 34, 64, 90]
+
+5. Testing Dynamic Programming:
+LIS length: 4
+6. Testing String Algorithms:
+KMP matches: [10]
+
+7. Testing Performance:
+Performance results:
+Size 100: {'bubble_sort': 0.00045013427734375, 'insertion_sort': 0.00028824806213378906, 'merge_sort': 0.00024271011352539062, 'quick_sort': 0.00016570091247558594, 'heap_sort': 3.695487976074219e-05}
+Size 500: {'bubble_sort': 0.013913631439208984, 'insertion_sort': 0.007479667663574219, 'merge_sort': 0.0012867450714111328, 'quick_sort': 0.0009140968322753906, 'heap_sort': 0.00019979476928710938}
+Size 1000: {'bubble_sort': 0.07345223426818848, 'insertion_sort': 0.03403949737548828, 'merge_sort': 0.0028769969940185547, 'quick_sort': 0.0017135143280029297, 'heap_sort': 0.000377655029296875}
+
+All exercises completed!
